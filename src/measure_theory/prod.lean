d7faeafb566fd020fafd52fe39be66d5c9f9160d
Floris van Doorn <fpvdoorn@gmail.com>
2020-08-20 03:57:52 -0400

    binary prod

--- src/measure_theory/prod.lean
+++ src/measure_theory/prod.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
-import measure_theory.measure_space
+import measure_theory.giry_monad
 
 /-!
 # The product measure space
@@ -134,6 +134,19 @@ mul_le_mul h (le_refl a)
 
 end canonically_ordered_semiring
 
+section monoid_with_zero
+
+@[simp] lemma zero_pow_eq_zero {α} [monoid_with_zero α] [nontrivial α] {n : ℕ} :
+  (0 : α) ^ n = 0 ↔ 0 < n :=
+begin
+  split; intro h,
+  { rw [nat.pos_iff_ne_zero], rintro rfl, simpa using h },
+  { exact zero_pow' n h.ne.symm }
+end
+
+
+end monoid_with_zero
+
 namespace finset
 @[to_additive]
 lemma prod_split {α β} [decidable_eq α] [comm_monoid β] (s t : finset α) (f : α → β) :
@@ -206,9 +219,72 @@ prod_add_prod_le' (mem_univ i) h2i (λ j _, hgf j) (λ j _, hhf j)
 
 end finset
 
+section complete_lattice
+
+variables {ι : Sort*} {α : Type*} {x : α} [complete_lattice α]
+lemma supr_const_le : (⨆ (h : ι), x) ≤ x :=
+supr_le (λ _, le_rfl)
+
+lemma le_infi_const : x ≤ (⨅ (h : ι), x) :=
+le_infi (λ _, le_rfl)
+
+end complete_lattice
+
 namespace measure_theory
 
-section
+namespace outer_measure
+section bounded_by
+
+variables {α : Type*} (m : set α → ennreal)
+
+/-- Given any function `m` assigning measures to sets, there is a unique maximal outer measure `μ`
+  satisfying `μ s ≤ m s` for all `s : set α`. This is the same as `outer_measure.of_function`,
+  except that it doesn't require `m ∅ = 0`. -/
+def bounded_by : outer_measure α :=
+outer_measure.of_function (λ s, ⨆ (h : s.nonempty), m s) (by simp [empty_not_nonempty])
+
+variables {m}
+theorem bounded_by_le (s : set α) : bounded_by m s ≤ m s :=
+(of_function_le _).trans supr_const_le
+
+local attribute [irreducible] outer_measure.of_function
+theorem bounded_by_eq_of_function (m_empty : m ∅ = 0) (s : set α) :
+  bounded_by m s = outer_measure.of_function m m_empty s :=
+begin
+  have : (λ s : set α, ⨆ (h : s.nonempty), m s) = m,
+  { ext1 t, cases t.eq_empty_or_nonempty with h h; simp [h, empty_not_nonempty, m_empty] },
+  simp [bounded_by, this]
+end
+
+theorem bounded_by_eq (s : set α) (m_empty : m ∅ = 0) (m_mono : ∀ ⦃t : set α⦄, s ⊆ t → m s ≤ m t)
+  (m_subadd : ∀ (s : ℕ → set α), m (⋃i, s i) ≤ (∑'i, m (s i))) : bounded_by m s = m s :=
+by rw [bounded_by_eq_of_function m_empty, of_function_eq s m_mono m_subadd]
+
+theorem le_bounded_by {μ : outer_measure α} : μ ≤ bounded_by m ↔ ∀ s, μ s ≤ m s :=
+begin
+  rw [bounded_by, le_of_function, forall_congr], intro s,
+  cases s.eq_empty_or_nonempty with h h; simp [h, empty_not_nonempty]
+end
+
+theorem le_bounded_by' {μ : outer_measure α} :
+  μ ≤ bounded_by m ↔ ∀ s : set α, s.nonempty → μ s ≤ m s :=
+by { rw [le_bounded_by, forall_congr], intro s, cases s.eq_empty_or_nonempty with h h; simp [h] }
+
+lemma bounded_by_caratheodory {m : set α → ennreal} {s : set α}
+  (hs : ∀t, m (t ∩ s) + m (t \ s) ≤ m t) : (bounded_by m).caratheodory.is_measurable s :=
+begin
+  apply of_function_caratheodory, intro t,
+  cases t.eq_empty_or_nonempty with h h,
+  { simp [h, empty_not_nonempty] },
+  { convert le_trans _ (hs t), simp [h], exact add_le_add supr_const_le supr_const_le }
+end
+
+/- TODO: also replace `Inf_eq_of_function_Inf_gen`. -/
+end bounded_by
+end outer_measure
+open outer_measure
+
+section measurable_pi
 variables {α : Type*} {β : α → Type*} [Πa, measurable_space (β a)]
 
 lemma is_measurable.pi [encodable α] {t : Π i, set (β i)} (hs : ∀ i, is_measurable (t i)) :
@@ -238,34 +314,43 @@ begin
   { simp [←not_nonempty_iff_eq_empty, pi_univ_nonempty_iff.mp h, is_measurable_pi_of_nonempty h] }
 end
 
-end
+end measurable_pi
+
+section measure_pi
 
 variables {ι : Type*} [fintype ι] {α : ι → Type*} {m : Π i, outer_measure (α i)}
 
 /-- An upper bound for the measure in a product space.
   It is defined to be the product of the measures of all its projections.
-  For boxes it should be equal to the correct measure
-  It is explicitly defined to be 0 for the empty set (which only matters if `ι` is empty. -/
+  For boxes it should be equal to the correct measure. -/
 def pi_premeasure (m : Π i, outer_measure (α i)) (s : set (Π i, α i)) : ennreal :=
-if s = ∅ then 0 else ∏ i, m i (eval i '' s)
+∏ i, m i (eval i '' s)
 
-@[simp] lemma pi_premeasure_empty : pi_premeasure m ∅ = 0 :=
-by simp [pi_premeasure]
-
-@[simp] lemma pi_premeasure_nonempty {s : set (Π i, α i)} (hs : s.nonempty) :
-  pi_premeasure m s = ∏ i, m i (eval i '' s) :=
-by simp [pi_premeasure, hs, ← not_nonempty_iff_eq_empty]
-
-lemma pi_premeasure_le_prod {s : set (Π i, α i)} :
-  pi_premeasure m s ≤ ∏ i, m i (eval i '' s) :=
-by { cases s.eq_empty_or_nonempty with h h; simp [h, le_refl] }
+@[simp] lemma pi_premeasure_def {s : set (Π i, α i)} :
+  pi_premeasure m s = ∏ i, m i (eval i '' s) := rfl
 
 lemma pi_premeasure_pi {s : Π i, set (α i)} (hs : (pi univ s).nonempty) :
   pi_premeasure m (pi univ s) = ∏ i, m i (s i) :=
 by simp [hs]
 
-lemma pi_premeasure_pi_le {s : Π i, set (α i)} : pi_premeasure m (pi univ s) ≤ ∏ i, m i (s i) :=
-by { cases (pi univ s).eq_empty_or_nonempty with h h; simp [h, le_refl] }
+lemma pi_premeasure_pi' [nonempty ι] {s : Π i, set (α i)} :
+  pi_premeasure m (pi univ s) = ∏ i, m i (s i) :=
+begin
+  cases (pi univ s).eq_empty_or_nonempty with h h,
+  { rcases pi_univ_eq_empty_iff.mp h with ⟨i, hi⟩,
+    have : ∃ i, m i (s i) = 0 := ⟨i, by simp [hi]⟩,
+    simpa [h, finset.card_univ, zero_pow (fintype.card_pos_iff.mpr _inst_2),
+      @eq_comm _ (0 : ennreal), finset.prod_eq_zero_iff] },
+  { simp [h] }
+end
+
+lemma pi_premeasure_pi_mono {s t : set (Π i, α i)} (h : s ⊆ t) :
+  pi_premeasure m s ≤ pi_premeasure m t :=
+finset.prod_le_prod' (λ i _, (m i).mono' (image_subset _ h))
+
+lemma pi_premeasure_pi_eval [nonempty ι] {s : set (Π i, α i)} :
+  pi_premeasure m (pi univ (λ i, eval i '' s)) = pi_premeasure m s :=
+by simp [pi_premeasure_pi']
 
 namespace outer_measure
 /-- `outer_measure.pi m` is the finite product of the outer measures `{m i | i : ι}`.
@@ -273,22 +358,33 @@ namespace outer_measure
   `n (pi univ s) ≤ ∏ i, m i (s i)`, where `pi univ s` is the product of the sets
   `{ s i | i : ι }`. -/
 protected def pi (m : Π i, outer_measure (α i)) : outer_measure (Π i, α i) :=
-outer_measure.of_function (pi_premeasure m) pi_premeasure_empty
+bounded_by (pi_premeasure m)
 
 lemma pi_pi_le (s : Π i, set (α i)) :
   outer_measure.pi m (pi univ s) ≤ ∏ i, m i (s i) :=
-le_trans (of_function_le _) pi_premeasure_pi_le
+by { cases (pi univ s).eq_empty_or_nonempty with h h, simp [h],
+     exact (bounded_by_le _).trans_eq (pi_premeasure_pi h) }
+
 
 lemma le_pi {m : Π i, outer_measure (α i)} {n : outer_measure (Π i, α i)} :
-  n ≤ outer_measure.pi m ↔ ∀ (s : Π i, set (α i)), n (pi univ s) ≤ ∏ i, m i (s i) :=
+  n ≤ outer_measure.pi m ↔ ∀ (s : Π i, set (α i)), (pi univ s).nonempty →
+    n (pi univ s) ≤ ∏ i, m i (s i) :=
 begin
-  rw [outer_measure.pi, le_of_function], split,
-  { intros h s, refine le_trans (h _) pi_premeasure_pi_le },
-  { intros h s,
-    cases s.eq_empty_or_nonempty with hs hs, { simp [hs] },
-    refine le_trans (n.mono $ subset_pi_eval_image univ s) _, simp [h, hs] }
+  rw [outer_measure.pi, le_bounded_by'], split,
+  { intros h s hs, refine (h _ hs).trans_eq (pi_premeasure_pi hs)  },
+  { intros h s hs, refine le_trans (n.mono $ subset_pi_eval_image univ s) (h _ _),
+    simp [pi_univ_nonempty_iff, hs] }
 end
 
+lemma pi_pi_false [encodable ι] (s : Π i, set (α i))
+  (h2s : (pi univ s).nonempty) : outer_measure.pi m (pi univ s) = ∏ i, m i (s i) :=
+begin
+  simp_rw [← pi_premeasure_pi h2s],
+  refine le_antisymm (bounded_by_le _) _,
+  refine le_binfi _, dsimp only, intros t ht,
+  sorry
+  -- refine le_trans _ (ennreal.tsum_le_tsum $ λ i, _),
+end
 end outer_measure
 
 namespace measure
@@ -304,10 +400,8 @@ lemma pi_caratheodory :
 begin
   refine supr_le _, intros i s hs,
   simp [measurable_space.comap] at hs, rcases hs with ⟨s, hs, rfl⟩,
-  apply of_function_caratheodory,
-  intro t, cases t.eq_empty_or_nonempty with h h, { simp [h] },
-  rw [pi_premeasure_nonempty h],
-  refine le_trans (add_le_add pi_premeasure_le_prod pi_premeasure_le_prod) _,
+  apply bounded_by_caratheodory, intro t,
+  simp_rw [pi_premeasure_def],
   refine finset.prod_univ_add_prod_univ_le' i _ _ _,
   { simp [image_inter_preimage, image_compl_preimage, (μ i).caratheodory hs, le_refl] },
   { intros j hj, apply mono', apply image_subset, apply inter_subset_left },
@@ -321,16 +415,64 @@ end
 protected def pi : measure (Π i, α i) :=
 to_measure (outer_measure.pi (λ i, (μ i).to_outer_measure)) (pi_caratheodory μ)
 
-lemma pi_pi (s : Π i, set (α i)) (hs : (pi univ s).nonempty) :
-  measure.pi μ (pi univ s) = ∏ i, μ i (s i) :=
+lemma pi_pi [encodable ι] (s : Π i, set (α i)) (h1s : ∀ i, is_measurable (s i))
+  (h2s : (pi univ s).nonempty) : measure.pi μ (pi univ s) = ∏ i, μ i (s i) :=
 begin
-
-  -- refine le_antisymm (outer_measure.pi_pi_le s) _,
+  rw [measure.pi, to_measure_apply _ _ (is_measurable.pi h1s)],
+  simp_rw [← to_outer_measure_apply, ← pi_premeasure_pi h2s],
+  refine le_antisymm (bounded_by_le _) _,
+  refine le_binfi _, dsimp only, intros t ht,
+  sorry
 end
 
+end measure
+
+end measure_pi
+
+
+/-! ### Prod -/
+namespace measure
+
+variables {α β : Type*} [measurable_space α] [measurable_space β] (μ : measure α) (ν : measure β)
+
+protected def prod : measure (α × β) :=
+bind μ $ λ x : α, map (prod.mk x) ν
+
+variables {μ ν}
+
+lemma measurable.map {ι} [measurable_space ι] {f : ι → α → β} (hf : ∀ i, measurable (f i)) :
+  measurable (λx, map (f x) μ) :=
+begin
+  sorry
+end
+
+@[simp] lemma prod_apply {s : set (α × β)} (hs : is_measurable s) :
+  μ.prod ν s = ∫⁻ x, ν (prod.mk x ⁻¹' s) ∂μ :=
+begin
+  rw [measure.prod, bind_apply hs],
+  congr, ext x : 1, rw [map_apply _ hs],
+  apply measurable_const.prod_mk measurable_id,
+  refine measurable.map (λ _, measurable_const.prod_mk measurable_id)
+end
+
+lemma prod_apply_symm {s : set (α × β)} (hs : is_measurable s) :
+  μ.prod ν s = ∫⁻ y, μ ((λ x, (x, y)) ⁻¹' s) ∂ν :=
+begin
+  /- prove for rectangles on which both `μ` and `ν` are finite -/
+  /- assume μ and ν σ-finite -/
+  /- show that it is closed under countable disjoint unions -/
+  /- show that this is sufficient -/
+  sorry
+end
+
+lemma prod_eq_symm : μ.prod ν = bind ν (λ y : β, map (λ x, (x, y)) μ) :=
+begin
+  simp [measure.prod, bind],
+  sorry
+end
 
 end measure
 
 end measure_theory
 
-#lint
+-- #lint
